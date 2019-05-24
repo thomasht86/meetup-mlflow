492e8611fd46695d39df2563f3299acb7174488d
thomasht86 <thomas.h.thoresen@gmail.com>
2019-05-24 13:01:48 +0200

    changed README.md

--- README.md
+++ README.md
@@ -1,9 +1,34 @@
 # meetup-mlflow
 Trondheim Machine Learning Meetup - MlFlow
 
-## Prerequisites
-* Windows subsystem for linux https://linuxconfig.org/how-to-install-ubuntu-18-04-on-windows-10
-* VS Code Insiders + Remote WSL extension https://code.visualstudio.com/docs/remote/wsl
+## Preparation
+This is a guide to help you set up your development environment so that you can follow the demo on your own computer. (And use MlFlow for your own projects afterwards)
+### Mac users
+1. Install Anaconda
+2. Install Visual Studio Code
+3. Install MlFlow
+You cannot install MLflow on the MacOS system installation of Python. We recommend installing Python 3 through the Homebrew package manager using brew install python. (In this case, installing MLflow is now pip3 install mlflow).
+
+### Windows 10
+MlFlow does not support installation on Windows (yet). But I will describe one way that you can make it work anyway, that may also be useful for other development scenarios. This is only possible for windows 10. For users with earlier windows versions, I do not have a solution, unfortunately.
+
+With Windows 10 came the opportunity to install Windows Subsystem for Linux (WSL). This enables us to access a full Linux environment without the overhead of a virtual machine. WSL is not enabled by default, but must be configured. Here is a straightforward recipe to achieve this. (Choose Ubuntu 18.04 as the Linux distribution from the Microsoft store)
+1. Install Anaconda in WSL your Ubuntu 18.04 shell according to these instructions
+2. Install Visual Studio Code Insiders (in Windows)
+3. Install Remote Development Extension Pack for Visual Studio Code
+This allows us to access the Linux filesystem from VS Code.
+
+4. Open the linux filesystem from VS Code 
+A)
+Open the Ubuntu 18.04 application. 
+Run the command:
+```code-insiders .```
+Necessary components will be installed, and VS Code with access to the WSL file system will be launched.
+OR
+B) 
+Open Visual Studio Code - Insiders, open the command palette (Ctrl+Shift+P) and choose "Remote WSL: New window".
+You should now be able to open a folder on the Ubuntu filesystem from Visual Studio Code Insiders. 
+
 ## Training 
 
 ## Comparing
--- train.py
+++ train.py
@@ -27,107 +27,109 @@ parser.add_argument("-n", "--ngram_range", help="Number of character sequences t
 parser.add_argument("-a", "--alpha", help="Naive bayes smoothing parameter (0=no smoothing)", type=float, default=1.)
 parser.add_argument("-f", "--fit_prior", help="Whether to fit priors", action="store_true", default=True)
 parser.add_argument("-u", "--use_stoplist", help="Whether to remove most common words", action="store_true", default=True)
-parser.add_argument("-d", "--datafile", help="Path to datafile", type=str, default="data/course_descriptions.csv")
+parser.add_argument("-d", "--datafile", help="Path to datafile", type=str, default="mlflow/data/course_descriptions.csv")
 
-args = parser.parse_args()
-print("Using arguments:")
-print(args)
-seed = args.seed
-use_idf = args.idf
-ngram_range = (1,args.ngram_range)
-alpha = args.alpha
-fit_prior = args.fit_prior
-use_stoplist = args.use_stoplist
-use_svd = False
+if __name__ == "__main__":
 
-conda_env_path = "environment.yml"
-model_prefix = "models"
-data_file = args.datafile
+    args = parser.parse_args()
+    print("Using arguments:")
+    print(args)
+    seed = args.seed
+    use_idf = args.idf
+    ngram_range = (1,args.ngram_range)
+    alpha = args.alpha
+    fit_prior = args.fit_prior
+    use_stoplist = args.use_stoplist
+    use_svd = False
 
-mlflow.set_experiment("ntnu_course_classifier")
+    conda_env_path = "environment.yml"
+    model_prefix = "models"
+    data_file = args.datafile
 
-## Data import and cleaning
-with mlflow.start_run() as run:
-    # Get path to save model
-    tracking_uri = mlflow.get_artifact_uri().strip("artifacts")
-    print("Logging to "+tracking_uri)
-    model_path = tracking_uri+model_prefix
+    mlflow.set_experiment("ntnu_course_classifier")
 
-    # Log params
-    mlflow.log_param("seed", seed)
-    mlflow.log_param("use_idf", use_idf)
-    mlflow.log_param("use_stoplist", use_stoplist)
-    mlflow.log_param("ngram_range", ngram_range)
-    mlflow.log_param("alpha", alpha)
-    mlflow.log_param("fit_prior", fit_prior)
-    mlflow.log_param("data_file", data_file)
+    ## Data import and cleaning
+    with mlflow.start_run() as run:
+        # Get path to save model
+        tracking_uri = mlflow.get_artifact_uri().strip("artifacts")
+        print("Logging to "+tracking_uri)
+        model_path = tracking_uri+model_prefix
 
-    df = pd.read_csv(data_file, usecols=[1,2,3,4,5,6])
-    df = df.dropna()
+        # Log params
+        mlflow.log_param("seed", seed)
+        mlflow.log_param("use_idf", use_idf)
+        mlflow.log_param("use_stoplist", use_stoplist)
+        mlflow.log_param("ngram_range", ngram_range)
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("fit_prior", fit_prior)
+        mlflow.log_param("data_file", data_file)
 
-    def remove_punctuation(document):
-        # Remove weird characters
-        return "".join([ (c if c not in string.punctuation+"\n\r\t" else " ") for c in document])
+        df = pd.read_csv(data_file, usecols=[1,2,3,4,5,6])
+        df = df.dropna()
 
-    def tokenize(document):
-        # From document to list of lower case tokens
-        return [w.lower() for w in remove_punctuation(document).split(" ") if len(w)>0]
+        def remove_punctuation(document):
+            # Remove weird characters
+            return "".join([ (c if c not in string.punctuation+"\n\r\t" else " ") for c in document])
 
-    if use_stoplist:
-        stoplist = [l.strip() for l in open("stopwords.txt", "r").readlines()]
-    else:
-        stoplist = None
+        def tokenize(document):
+            # From document to list of lower case tokens
+            return [w.lower() for w in remove_punctuation(document).split(" ") if len(w)>0]
 
-    # Create pandas Series with target variable 
-    y = df["fac"].astype(str)
+        if use_stoplist:
+            stoplist = [l.strip() for l in open("stopwords.txt", "r").readlines()]
+        else:
+            stoplist = None
 
-    # Split into training and test set
-    X_train, X_test, y_train, y_test = train_test_split(df["description"], y, stratify=y, random_state=seed)
+        # Create pandas Series with target variable 
+        y = df["fac"].astype(str)
 
-    # Initialize the TfidfVectorizer
-    vec = TfidfVectorizer(tokenizer=tokenize, stop_words=stoplist, use_idf=use_idf, ngram_range=ngram_range)
-    
-    if use_svd:
-        svd = TruncatedSVD(n_components=100, n_iter=50)
-        scaler = MinMaxScaler()
-        vec = make_pipeline(vec, svd, scaler)
+        # Split into training and test set
+        X_train, X_test, y_train, y_test = train_test_split(df["description"], y, stratify=y, random_state=seed)
 
-    # Fit to training set and transform test set
-    trn_vec= vec.fit_transform(X_train.values)
-    test_vec = vec.transform(X_test.values)
+        # Initialize the TfidfVectorizer
+        vec = TfidfVectorizer(tokenizer=tokenize, stop_words=stoplist, use_idf=use_idf, ngram_range=ngram_range)
+        
+        if use_svd:
+            svd = TruncatedSVD(n_components=100, n_iter=50)
+            scaler = MinMaxScaler()
+            vec = make_pipeline(vec, svd, scaler)
 
-    # Create a list of the unique target values
-    label_cols = df["fac"].astype(str).unique().tolist()
+        # Fit to training set and transform test set
+        trn_vec= vec.fit_transform(X_train.values)
+        test_vec = vec.transform(X_test.values)
 
-    # Initialize a Naive Bayes-classifier
-    clf = MultinomialNB(alpha=alpha, fit_prior=fit_prior)
+        # Create a list of the unique target values
+        label_cols = df["fac"].astype(str).unique().tolist()
 
-    # Fit the model
-    clf.fit(trn_vec, y_train)
+        # Initialize a Naive Bayes-classifier
+        clf = MultinomialNB(alpha=alpha, fit_prior=fit_prior)
 
-    # Make predictions with test set
-    preds = clf.predict(test_vec)
-    
-    # Calculate metrics from predictions and y_test
-    acc = accuracy_score(y_test, preds)
-    f1_macro = precision_score(y_test, preds, average="macro")
-    f1_micro = precision_score(y_test, preds, average="micro")
-    f1_per_class = precision_score(y_test, preds, labels=label_cols, average=None)
+        # Fit the model
+        clf.fit(trn_vec, y_train)
 
-    mlflow.log_metric("accuracy", acc)
-    mlflow.log_metric("f1_macro", f1_macro)
-    mlflow.log_metric("f1_micro", f1_micro)
-    for c, f1 in zip(label_cols, f1_per_class):
-        mlflow.log_metric("f1_"+c, f1)
-    
-    mlflow.sklearn.save_model(clf, model_path, conda_env=conda_env_path, serialization_format=mlflow.sklearn.SERIALIZATION_FORMAT_CLOUDPICKLE)
+        # Make predictions with test set
+        preds = clf.predict(test_vec)
+        
+        # Calculate metrics from predictions and y_test
+        acc = accuracy_score(y_test, preds)
+        f1_macro = precision_score(y_test, preds, average="macro")
+        f1_micro = precision_score(y_test, preds, average="micro")
+        f1_per_class = precision_score(y_test, preds, labels=label_cols, average=None)
 
-    #%% [markdown]
-    # ## Next steps
-    # - Define metrics
-    # - Plot
-    # - Script evolution
-    # - Think about steps
+        mlflow.log_metric("accuracy", acc)
+        mlflow.log_metric("f1_macro", f1_macro)
+        mlflow.log_metric("f1_micro", f1_micro)
+        for c, f1 in zip(label_cols, f1_per_class):
+            mlflow.log_metric("f1_"+c, f1)
+        
+        mlflow.sklearn.save_model(clf, model_path, conda_env=conda_env_path, serialization_format=mlflow.sklearn.SERIALIZATION_FORMAT_CLOUDPICKLE)
+
+        #%% [markdown]
+        # ## Next steps
+        # - Define metrics
+        # - Plot
+        # - Script evolution
+        # - Think about steps
 
 
 
